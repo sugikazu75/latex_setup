5e31c4d906eb6bcf1df1cac19a96a2e22b87a54e
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2022-07-13 17:59:25 +0900

    rename images to figs

--- update_figs.sh
+++ update_figs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-rm -rf image/*
+CURRENT=$(cd $(dirname $0);pwd)
+echo $CURRENT
+cd $CURRENT
+
+rm -rf figs/*
 gdown https://drive.google.com/drive/folders/1UydZM6qkAej4TB1zSpcz9eGU0-wRWeOS --folder
